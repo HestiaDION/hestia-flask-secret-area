39290fb15cb6821eb87d063542503224e92c4146
cruzanab <ana.cruz@germinare.org.br>
2024-11-06 09:53:20 -0300

    Updating script

--- app.py
+++ app.py
@@ -70,8 +70,5 @@ def login():
         logging.error(f"Erro inesperado: {e}")
         return jsonify({'error': 'Erro inesperado no servidor'}), 500
 
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Use a porta fornecida pelo Render
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
